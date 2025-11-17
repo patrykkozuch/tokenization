122825d96412f0a7845def41bfd10d5482999612
Patryk Kożuch <patryk.kozuch.1@gmail.com>
2025-11-17 20:58:27 +0100

    Train tokenizers on wider datasets

--- custom_tokenizers/bocian.py
+++ custom_tokenizers/bocian.py
@@ -10,6 +10,6 @@ tokenizer = Tokenizer(models.BPE(unk_token="<UNK>", dropout=0.1, fuse_unk=True))
 
 tokenizer.normalizer = normalizers.NFKC()
 tokenizer.pre_tokenizer = pre_tokenizers.Metaspace()
-tokenizer.decoder = decoders.Sequence([decoders.Metaspace(), decoders.Strip(" ", 1, 0)])
+tokenizer.decoder = decoders.Metaspace()
 
 BPE_TOKENIZER = tokenizer
--- custom_tokenizers/corpus.py
+++ custom_tokenizers/corpus.py
@@ -1,4 +1,8 @@
-from datasets import load_dataset
+import glob
+
+from datasets import load_dataset, Features, Value, List
+from sympy.integrals.meijerint_doc import category
+from tokenizers.implementations import SentencePieceBPETokenizer
 
 
 def get_training_corpus(corpus_path: str):
@@ -20,9 +24,10 @@ def get_training_corpus(corpus_path: str):
         r'[a-zA-ZąćęłńóśźżĄĆĘŁŃÓŚŹŻ0-9\s.,!?;:\-\'\"()\[\]{}/@#$%&*+=<>_~`|\\]+'
     )
 
+    data_files = list(glob.glob(corpus_path))
     dataset = (
-        load_dataset('json', data_files=[corpus_path], split="train")
-        .filter(lambda x: x["meta"]["quality"] == "HIGH")
+        load_dataset('json', data_files=data_files, split="train", num_proc=20)
+        .filter(lambda x: x.get("meta", {}).get("quality", "") == "HIGH", num_proc=20)
     )
 
     for i in range(0, len(dataset), 1000):
